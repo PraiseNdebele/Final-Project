97951be727f7e974891908972e594a71bc9fc572
danielzzhao@gmail.com <danielzzhao@gmail.com>
2015-07-08 10:41:55 +0200

    praise working version with email verification

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707161712) do
+ActiveRecord::Schema.define(version: 20150708083506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,10 @@ ActiveRecord::Schema.define(version: 20150707161712) do
     t.string   "career_field"
     t.text     "bio"
     t.integer  "phone"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
